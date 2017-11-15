96232b65cf79b9b5f7df28a405fbaf15bae902ce
John McClure <jwmcclure93@gmail.com>
2017-11-15 17:05:45 -0600

    OrderController events

--- src/application/controller/OrderController.java
+++ src/application/controller/OrderController.java
@@ -30,8 +30,53 @@ public class OrderController implements EventHandler<ActionEvent> {
 		Scene viewScene = new Scene(viewParent);
 		window.setScene(viewScene);
 	}
-		
+	
+	public void AddFoodToTable(ActionEvent event) {
+		Button b = (Button)event.getSource();
+		String str = b.getText();
+		if(str.equals("Crunchy Taco W/Beef")) {
+			System.out.println(str);
+		}
+		else if(str.equals("Soft Taco W/Beef")) {
+			System.out.println(str);
+		}
+		else if(str.equals("Soft Taco W/Chicken")) {
+			System.out.println(str);
+		}
+		else if(str.equals("Crunchy Taco W/Chicken")) {
+			System.out.println(str);
+		}
+		else if(str.equals("Beef Burrito")) {
+			System.out.println(str);
+		}
+		else if(str.equals("Chicken Burrito")) {
+			System.out.println(str);
+		}
+		else {
+			System.out.println("Item not found");
+		}
+	}
 
+	public void AddDrinkToTable(ActionEvent event) {
+		Button b = (Button)event.getSource();
+		String str = b.getText();
+		if(str.equals("Soft Drink")) {
+			System.out.println(str);
+		}
+		else if(str.equals("Water")) {
+			System.out.println(str);
+		}
+		else if(str.equals("Tea")) {
+			System.out.println(str);
+		}
+		else if(str.equals("Lemonade")) {
+			System.out.println(str);
+		}
+		else {
+			System.out.println("Item not found");
+		}
+	}
+	
 	@Override
 	public void handle(ActionEvent arg0) {
 		// TODO Auto-generated method stub
