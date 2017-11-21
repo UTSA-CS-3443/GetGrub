a383fe3b3739475f6c58fcde172d486707b54d63
BRiNK <BRiNK@172.24.69.134>
2017-11-21 10:34:16 -0600

    worked on delivery table, order buttons, and other inventory changes

--- src/application/controller/InventoryController.java
+++ src/application/controller/InventoryController.java
@@ -1,8 +1,12 @@
 package application.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import application.model.Inventory;
 import application.model.Order;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -11,6 +15,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 public class InventoryController  implements EventHandler<ActionEvent> {
@@ -39,6 +45,16 @@ public class InventoryController  implements EventHandler<ActionEvent> {
 	@FXML
 	private Label lemonStock;
 	
+	@FXML
+	private TableView<Inventory> deliveryTable;
+	
+	@FXML
+	private TableColumn<Inventory, String> stockInfo;
+	
+	//@FXML
+	
+	
+	
 	private double beefT = 50.00;
 	private double chickenT = 35.00;
 	private double shellT = 15.00;
@@ -51,6 +67,10 @@ public class InventoryController  implements EventHandler<ActionEvent> {
 	/**
 	 * constructor sets up a Calculator
 	 */
+
+	private ArrayList<Inventory> currentInventories;
+	private final ObservableList<Inventory> inventories = FXCollections.observableArrayList();
+	
 	public InventoryController() {
 		super();
 	}
@@ -58,7 +78,9 @@ public class InventoryController  implements EventHandler<ActionEvent> {
 	@FXML
 	public void initialize() {
 		updateLabels();
-		
+		stockInfo.setCellValueFactory(cellData -> cellData.getValue().menuItemProperty());
+		deliveryTable.setItems(inventories);
+		deliveryTable.setPlaceholder(new Label(""));
 	}
 	
 	public void returnHomeButton(ActionEvent event) throws IOException
@@ -75,30 +97,51 @@ public class InventoryController  implements EventHandler<ActionEvent> {
 		// TODO Auto-generated method stub
 		
 	}
-	public void orderItemForDelivery()
+	public void orderItemForDelivery(ActionEvent event)
 	{
-		System.out.print("I do nothing right now");
+		//System.out.print("I do nothing right now");
+		Button b = (Button)event.getSource();
+		String str = b.getText();
+		if(str.equals("Order Beef")) {
+			System.out.println(str);
+			Inventory newItem = new Inventory("beef 25lb", 25.00);
+			currentInventories.add(newItem);
+			inventories.add(newItem);
 	}
+}
 	
 	
 	public void updateLabels() {
 		
 		this.beefStock.setText(String.format("%.2f", (beefT)));
-		//this.chickenStock.setText(String.format("%.2f", chickenT));
-		//this.hardSStock.setText(String.format("%.2f", (shellT)));
-		//this.tortillaStock.setText(String.format("%.2f", (tortillaT)));
-		//this.lettuceStock.setText(String.format("%.2f", (lettuceT)));
-		//this.cheeseStock.setText(String.format("%.2f", (cheeseT)));
-		//this.teaStock.setText(String.format("%.2f", (teaT)));
-		//this.lemonStock.setText(String.format("%.2f", (lemonT)));
+		this.chickenStock.setText(String.format("%.2f", chickenT));
+		this.hardSStock.setText(String.format("%.2f", (shellT)));
+		this.tortillaStock.setText(String.format("%.2f", (tortillaT)));
+		this.lettuceStock.setText(String.format("%.2f", (lettuceT)));
+		this.cheeseStock.setText(String.format("%.2f", (cheeseT)));
+		this.teaStock.setText(String.format("%.2f", (teaT)));
+		this.lemonStock.setText(String.format("%.2f", (lemonT)));
 	}
 	
-	/*public addToTotal(String itemName) {
+	/*
+	 * public addToTotal(String itemName) {
 	if  (itemName == "beef"){
 	beefT = beefT + itemAmount;
 	} else if (itemName == "chicken"){ 
-	//etc...
-	 }
+	chickenT = chickenT + itemAmount;
+	 }else if (itemName == "hard shell"){
+	 shellT = shellT + itemAmount;
+	} else if (itemName == "tortilla"){
+	 tortillaT = tortillaT + itemAmount;
+	} else if (itemName == "lettuce"){
+	lettuceT = lettuceT + itemAmount;
+	} else if (itemName == "cheese"){
+	cheeseT = cheeseT + itemAmount;
+	} else if (itemName == "tea"){
+	teaT = teaT + itemAmount;
+	} else if (itemName == "lemon"){
+	lemonT = lemonT + itemAmount;
 	}
+	updateLabels();
 */
 }
--- src/application/model/Inventory.java
+++ src/application/model/Inventory.java
@@ -1,10 +1,49 @@
 package application.model;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Inventory {
 	
 	//public displayInv(){
 	//}
-	
-	
-
-}
+	//public orderItem(deliveries){}
+	//public updateInv(){}
+		
+		private StringProperty menuItem;
+		private	final DoubleProperty amount;
+		
+		
+		
+		public Inventory(String itemOrder,double itemAmount) {
+			this.menuItem = new SimpleStringProperty(itemOrder);
+			this.amount = new SimpleDoubleProperty(itemAmount);
+			
+		}
+		
+		public void setAmount(double iAmount) {
+			this.amount.set(iAmount);
+		}
+		
+		public double getAmount() {
+			return amount.get();
+		}
+		
+		public DoubleProperty amountProperty() {
+			return amount;
+		}
+		
+		public void setMenuItem(String iOrder) {
+			this.menuItem.set(iOrder);
+		}
+		
+		public String getMenuItem() {
+			return menuItem.get();
+		}
+		
+		public StringProperty menuItemProperty() {
+			return menuItem;
+		}
+	}
